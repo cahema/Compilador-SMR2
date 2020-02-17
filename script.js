2d6c81d730e462de4c7953d98cc9f876e910657a
Carlos Herrera <pokemana2@hotmail.com>
2020-02-18 00:36:00 +0100

    Ejecutar ya convertido
    Ejecutar se ha adaptado para usarse con el objeto ProcSMR2

--- script.js
+++ script.js
@@ -13,11 +13,24 @@
 
 function generar() {
 	
-	///////////////////
-	// Función en si //
-	///////////////////
+	function comprobarErrores() {
+		if (ProcSMR2.banderas.instruccionIlegal) { //Si hay una instrucción ilegal, muestra un error y la línea del error
+			divErrorCodigo.innerHTML = "Error: ha introducido una instrucción ilegal en la línea " + lineaError;
+			return;
+		}
+		
+		else if (ProcSMR2.banderas.registroIlegal) { //Si hay un registro ilegal, muestra un error y la línea error
+			divErrorCodigo.innerHTML = "Error: ha introducido un registro ilegal en la línea " + lineaError;
+			return;
+		}
+		
+		else if (ProcSMR2.banderas.numeroIlegal) { //Si hay un número ilegal, muestra un error y la línea del error
+			divErrorCodigo.innerHTML = "Error: ha introducido un número ilegal en la línea " + lineaError;
+			return;
+		}
+	}
 	
-	let txtCodigo = document.querySelector("#txtCodigo").value; //Recuperamos el texto en el textarea de código
+	let txtCodigo = document.querySelector("#txtCodigo").value.toLowerCase(); //Recuperamos el texto en el textarea de código
 	let divErrorCodigo = document.querySelector("#divErrorCodigo");
 
 	if (txtCodigo == "") { //Si está vacío, muestra un error
@@ -35,24 +48,9 @@ function generar() {
 	
 	while (contador < arrCodigo.length) {
 		
-		if (ProcSMR2.banderas.instruccionIlegal) { //Si hay una instrucción ilegal, muestra un error y la línea del error
-			divErrorCodigo.innerHTML = "Error: ha introducido una instrucción ilegal en la línea " + lineaError;
-			return;
-		}
-		
-		else if (ProcSMR2.banderas.registroIlegal) { //Si hay un registro ilegal, muestra un error y la línea error
-			divErrorCodigo.innerHTML = "Error: ha introducido un registro ilegal en la línea " + lineaError;
-			return;
-		}
-		
-		else if (ProcSMR2.banderas.numeroIlegal) { //Si hay un número ilegal, muestra un error y la línea del error
-			divErrorCodigo.innerHTML = "Error: ha introducido un número ilegal en la línea " + lineaError;
-			return;
-		}
-		
+		comprobarErrores();
 		lineaError++;
 		let instruccionActual = ProcSMR2.diccionarios.instrucciones[arrCodigo[contador]];
-		console.log(instruccionActual);
 		
 		if (instruccionActual == undefined) { ProcSMR2.banderas.instruccionIlegal = true; continue; }
 		else { strBinario += instruccionActual["codigoBinario"]; contador++; }
@@ -98,97 +96,9 @@ function generar() {
 
 function ejecutar() {
 
-	/*
-	* binarioADecimal
-	*
-	*	Esta función recibe un número en binario y devuelve el valor
-	*	decimal de ese número
-	*
-	*/
-
-
-	function binarioADecimal (numeroBinario) { //Función para convertir de binario a decimal
-
-		let suma = 0;
-		let x = 0;
-
-		for (let i = numeroBinario.length-1; i >= 0; i--) {
-
-			suma += Number(numeroBinario[x])*2**i; //2 elevado a la posición i del número - 1
-			x++;
-		}
-
-		if (suma > 255 || suma < 0) {
-			numeroIlegal = true;
-			return;
-		}
-
-		return suma;
-	}
-
-	/*
-	* ejecutarInstruccion
-	*
-	*	Esta función recibe un número en binario como parámetro, lo compara con las instrucciones
-	*	válidas y ejectua las acciones necesarias
-	*
-	*/
-
-
-	function ejecutarInstruccion (instruccion) {
-
-		if (instruccion == "00000") { //imprime
-
-			strOutput += registros[binarioADecimal(memoria[linea][1])];
-		}
-
-		else if (instruccion == "00001") { //imprimec
-
-			strOutput += caracteresASCII[registros[binarioADecimal(memoria[linea][1])]];
-		}
-
-		else if (instruccion == "00010") { //valor
-
-			registros[binarioADecimal(memoria[linea][1])] = binarioADecimal(memoria[linea][2]);
-		}
-
-		else if (instruccion == "00011") { //borra
-
-			registros[binarioADecimal(memoria[linea][1])] = 0;
-		}
-
-		else if (instruccion == "00100") { //suma
-
-			registros[binarioADecimal(memoria[linea][1])] += binarioADecimal(memoria[linea][2]);
-		}
-
-		else if (instruccion == "00101") { //resta
-
-			registros[binarioADecimal(memoria[linea][1])] -= binarioADecimal(memoria[linea][2]);
-		}
-
-		else if (instruccion == "00110") { //salta
-
-			linea = binarioADecimal(memoria[linea][2])-1;
-		}
-
-		else if (instruccion == "00111") { //saltasi0
-
-			if (registros[binarioADecimal(memoria[linea][1])] == 0) {
-
-				linea = binarioADecimal(memoria[linea][2])-1;
-			}
-		}
-
-		else {
-			instruccionIlegal = true;
-		}
-
-	}
-
-	let txtBinario = document.querySelector("#txtBinario").value;
+	let txtBinario      = document.querySelector("#txtBinario").value;
 	let divErrorBinario = document.querySelector("#divErrorBinario");
-	let divOutput = document.querySelector("#divOutput");
+	let divOutput       = document.querySelector("#divOutput");
 
 	if (txtBinario.length > 4096) { //Si el programa es más de 512 bytes de largo, muestra un error
 		divErrorBinario.innerHTML = "Error: el programa supera la longitud de 512 bytes";
@@ -200,6 +110,11 @@ function ejecutar() {
 		return;
 	}
 
+	else if (txtBinario.length % 16 != 0) {
+		divErrorBinario.innerHTML = "Error, el programa tiene una longitud inválida";
+		return;
+	}
+	
 	else { //Si no hay errores, vacía el div del output y el de errores
 		divOutput.innerHTML = "";
 		divErrorBinario.innerHTML = "";
@@ -215,40 +130,51 @@ function ejecutar() {
 		//Cada linea contiene una instrucción, registro y dato
 		if (ProcSMR2.memoria.programa[linea]["operacion"] == undefined) { 
 			ProcSMR2.memoria.programa[linea]["operacion"] = "";
-			ProcSMR2.memoria.programa[linea]["registro"] = "";
-			ProcSMR2.memoria.programa[linea]["dato"] = "";
+			ProcSMR2.memoria.programa[linea]["registro"]  = "";
+			ProcSMR2.memoria.programa[linea]["dato"]      = "";
 		}
 
 		for (let i = 0; i < 16 ; i++) {
 
 			if      (i < 5)  { ProcSMR2.memoria.programa[linea]["operacion"] += txtBinario[contador]; } //Carga la instrucción, 5 bits de largo
 
-			else if (i < 8)  { ProcSMR2.memoria.programa[linea]["registro"] += txtBinario[contador]; } //Carga el registro, 3 bits de largo
+			else if (i < 8)  { ProcSMR2.memoria.programa[linea]["registro"]  += txtBinario[contador]; } //Carga el registro, 3 bits de largo
 
-			else if (i < 16) { ProcSMR2.memoria.programa[linea]["dato"] += txtBinario[contador]; } //Carga el dato, 8 bits de largo
+			else if (i < 16) { ProcSMR2.memoria.programa[linea]["dato"]      += txtBinario[contador]; } //Carga el dato, 8 bits de largo
 
 			contador++;
 		}
 	}
 
-	console.table(ProcSMR2.memoria.programa);
 	ProcSMR2.memoria.linea = 0;
+	ProcSMR2.banderas.instruccionIlegal = false;
+	ProcSMR2.banderas.registroIlegal    = false;
+	ProcSMR2.banderas.numeroIlegal      = false;
 	
-	divOutput.innerHTML = "";
-	strOutput = "<pre>";
+	let strOutput = "";
 	let instruccionIlegal = false; //Bandera para señalar una instrucción ilegal
 	let numeroIlegal = false; //Bandera para señalar un número ilegal
-
+	strOutput = "<pre>";
+	divOutput.innerHTML = "";
+	
 	//Se repite el bucle hasta que llegue al final del programa o a la linea 255
 	while (ProcSMR2.memoria.linea < ProcSMR2.memoria.programa.length && ProcSMR2.memoria.linea <= 255) {
 
-		if (instruccionIlegal || numeroIlegal) { //Si hay una instrucción ilegal, muestra un error y la línea del error
+		if (instruccionIlegal) { //Si hay una instrucción ilegal, muestra un error y la línea del error
 			divErrorBinario.innerHTML = "Error: el código binario que ha introducido no es válido";
 			return;
 		}
 
 		//Ejecuta la instrucción (la cuál siempre está en el primer elemento) de la linea actual
-		ejecutarInstruccion(memoria[linea][0]);
+		if (ProcSMR2.auxiliares.operacionActual() == undefined) {
+			ProcSMR2.banderas.instruccionIlegal = true; continue;
+		}
+		else {
+			let resultadoOperacion = ProcSMR2.operaciones[ProcSMR2.auxiliares.operacionActual()]();
+			if (resultadoOperacion != undefined) {
+				strOutput += resultadoOperacion;
+			}
+		}
 		ProcSMR2.memoria.linea++;
 	}
 
@@ -262,7 +188,6 @@ function ejecutar() {
 //      MAIN      //
 ////////////////////
 
-let strOutput = "";
 let caracteresASCII = ["\u0000","\u0001","\u0002","\u0003","\u0004","\u0005","\u0006","\u0007","\b","\t","\n","\u000b","\f","\r","\u000e","\u000f","\u0010","\u0011","\u0012","\u0013","\u0014","\u0015","\u0016","\u0017","\u0018","\u0019","\u001a","\u001b","\u001c","\u001d","\u001e","\u001f"," ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/","0","1","2","3","4","5","6","7","8","9",":",";","<","=",">","?","@","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","[","\\","]","^","_","`","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","{","|","}","~"];
 
 document.querySelector("#btnGenerar").addEventListener("click", generar);
@@ -287,14 +212,14 @@ let ProcSMR2 = {
 		
 		registros : {
 			
-			R0 : "000",
-			R1 : "001",
-			R2 : "010",
-			R3 : "011",
-			R4 : "100",
-			R5 : "101",
-			R6 : "110",
-			R7 : "111",
+			r0 : "000",
+			r1 : "001",
+			r2 : "010",
+			r3 : "011",
+			r4 : "100",
+			r5 : "101",
+			r6 : "110",
+			r7 : "111",
 			
 		},
 	
@@ -311,13 +236,38 @@ let ProcSMR2 = {
 	operaciones : {
 		
 		"00000" : function() { //imprime
-        	strOutput += ProcSMR2.memoria.registros[parseInt(registroActual(), 2)];
+        	return ProcSMR2.memoria.registros[ProcSMR2.auxiliares.registroActual()];
     	},
 
     	"00001" : function() { //imprimec
-
+			return caracteresASCII[ProcSMR2.memoria.registros[ProcSMR2.auxiliares.registroActual()]];
+    	},
+		
+		"00010" : function() { //valor
+			ProcSMR2.memoria.registros[ProcSMR2.auxiliares.registroActual()] = ProcSMR2.auxiliares.datoActual();
     	},
 
+		"00011" : function() { //borra
+			ProcSMR2.memoria.registros[ProcSMR2.auxiliares.registroActual()] = 0;
+    	},
+		
+		"00100" : function() { //suma
+			ProcSMR2.memoria.registros[ProcSMR2.auxiliares.registroActual()] += ProcSMR2.auxiliares.datoActual();
+    	},
+		
+		"00101" : function() { //resta
+			ProcSMR2.memoria.registros[ProcSMR2.auxiliares.registroActual()] -= ProcSMR2.auxiliares.datoActual();
+    	},
+		
+		"00110" : function() { //salta
+			ProcSMR2.memoria.linea = ProcSMR2.auxiliares.datoActual()-1;
+    	},
+		
+		"00111" : function() { //saltasi0
+			if (ProcSMR2.memoria.registros[ProcSMR2.auxiliares.registroActual()] == 0) {
+				ProcSMR2.memoria.linea = ProcSMR2.auxiliares.datoActual()-1;
+			}
+    	},
     },
 	
 	banderas : {
@@ -330,14 +280,18 @@ let ProcSMR2 = {
 	
     auxiliares : {
 
+		operacionActual : function() {
+			return ProcSMR2.memoria.programa[ProcSMR2.memoria.linea]["operacion"];
+		},
+	
     	registroActual : function() {
-			let registroBin = ProcSMR2.memoria.programa[ProcSMR2.memoria.linea]["registro"]
+			let registroBin = ProcSMR2.memoria.programa[ProcSMR2.memoria.linea]["registro"];
     		return parseInt(registroBin, 2);
     	},
 
     	datoActual : function() {
-    		let registroBin = ProcSMR2.memoria.programa[ProcSMR2.memoria.linea]["dato"]
-    		return parseInt(registroBin, 2);
+    		let datoBin = ProcSMR2.memoria.programa[ProcSMR2.memoria.linea]["dato"];
+    		return parseInt(datoBin, 2);
     	},
 
     },
